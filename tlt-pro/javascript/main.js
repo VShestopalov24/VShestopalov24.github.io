20d887d8b439dd19e62b2e7fa709435d2ae997bf
VShestopalov24 <100041152+VShestopalov24@users.noreply.github.com>
2023-11-23 20:46:40 +0400

    Add files via upload

---
+++ tlt-pro/javascript/main.js
@@ -0,0 +1,56 @@
+const loginInput = document.getElementById('loginInput');
+const nameInput = document.getElementById('nameInput');
+const phoneInput = document.getElementById('phoneInput');
+const emailInput = document.getElementById('emailInput');
+
+nameInput.addEventListener('input', () => {
+  nameInput.setCustomValidity('');
+  nameInput.checkValidity();
+});
+
+nameInput.addEventListener('invalid', () => {
+  if(nameInput.value === '') {
+    nameInput.setCustomValidity('Введите свое имя!');
+  } else {
+    nameInput.setCustomValidity('Имя может содержать только латиницу. Повторите ещё раз!');
+  }
+});
+
+phoneInput.addEventListener('input', () => {
+    phoneInput.setCustomValidity('');
+    phoneInput.checkValidity();
+});
+
+phoneInput.addEventListener('invalid', () => {
+    if(phoneInput.value === '') {
+        phoneInput.setCustomValidity('Введите свой номер телефона!');
+    } else {
+        phoneInput.setCustomValidity('Номер должен иметь формат +70000000000. Повторите ещё раз!');
+    }
+});
+
+emailInput.addEventListener('input', () => {
+    emailInput.setCustomValidity('');
+    emailInput.checkValidity();
+});
+
+emailInput.addEventListener('invalid', () => {
+    if(emailInput.value === '') {
+        emailInput.setCustomValidity('Введите свой E-mail!');
+    } else {
+        emailInput.setCustomValidity('Номер должен иметь формат login@site.com. Повторите ещё раз!');
+    }
+});
+
+loginInput.addEventListener('input', () => {
+    loginInput.setCustomValidity('');
+    loginInput.checkValidity();
+});
+
+loginInput.addEventListener('invalid', () => {
+    if(loginInput.value === '') {
+        loginInput.setCustomValidity('Введите свой E-mail! или телефон');
+    } else {
+        loginInput.setCustomValidity('Номер должен иметь формат login@site.com или +70000000000. Повторите ещё раз!');
+    }
+});
